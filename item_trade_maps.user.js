96b62a2c5f46a29b58ee3ba221b5b5fc7bb3a5e1
Mike Chelen <mike@michaelchelen.net>
2010-06-04 03:56:21 -0400

    add item_trade_maps script for galactic fleets

---
+++ item_trade_maps.user.js
@@ -0,0 +1,5 @@
+// ==UserScript==
+// @name           Item Trade Maps
+// @namespace      http://userscripts.org/users/80737
+// @include        http://g1.galacticfleets.com/index.php?go=prices
+// ==/UserScript==
